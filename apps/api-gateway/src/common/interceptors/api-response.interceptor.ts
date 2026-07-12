b1a836d6dcd6092ef7a3c549fe7bec4d3241cff5
Mohammed Aashik <aashikdevelop@gmail.com>
2026-07-13 01:31:52 +0530

    Add ApiResponseInterceptor to main.ts for enhanced response handling
    - Introduced ApiResponseInterceptor to standardize API responses across the application.
    - Reinstated GrpcExceptionFilter for consistent error handling.
    - Updated global middleware configuration in the bootstrap function.

---
+++ apps/api-gateway/src/common/interceptors/api-response.interceptor.ts
@@ -0,0 +1,37 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Request, Response } from 'express';
+import { ApiResponse } from '../interfaces/api-response.interface';
+
+@Injectable()
+export class ApiResponseInterceptor<T> implements NestInterceptor<
+  T,
+  ApiResponse<T>
+> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<T>,
+  ): Observable<ApiResponse<T>> {
+    const ctx = context.switchToHttp();
+
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+
+    return next.handle().pipe(
+      map((data) => ({
+        success: true,
+        statusCode: response.statusCode,
+        message: 'Request successful',
+        data,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      })),
+    );
+  }
+}
--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -3,14 +3,13 @@ import { ApiGatewayModule } from './api-gateway.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { GrpcExceptionFilter } from './common/filters/grpc-exception.filter';
+import { ApiResponseInterceptor } from './common/interceptors/api-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('API_GATEWAY_PORT') || 3000;
-
-  app.useGlobalFilters(new GrpcExceptionFilter());
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -19,6 +18,10 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.useGlobalFilters(new GrpcExceptionFilter());
+
+  app.useGlobalInterceptors(new ApiResponseInterceptor());
 
   await app.listen(port);
   console.log(`API Gateway running on port ${port}`);
